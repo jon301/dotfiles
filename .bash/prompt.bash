d58141331a1d29d6897f6b6f747256b522871595
Jonathan Trang <jtrang@hubside.com>
2019-10-21 10:04:47 +0200

    prompt: remove time from ps1

--- .bash/prompt.bash
+++ .bash/prompt.bash
@@ -90,15 +90,15 @@ Jobs="\j"
 # This PS1 snippet was adopted from code for MAC/BSD I saw from: http://allancraig.net/index.php?option=com_content&view=article&id=108:ps1-export-command-for-git&catid=45:general&Itemid=96
 # I tweaked it to work on UBUNTU 11.04 & 11.10 plus made it mo' better
 
-export PS1=$IBlack$Time12h$Color_Off'$(git branch &>/dev/null;\
+export PS1=$IBlack$Color_Off'$(git branch &>/dev/null;\
 if [ $? -eq 0 ]; then \
   echo "$(echo `git status` | grep "nothing to commit" > /dev/null 2>&1; \
   if [ "$?" -eq "0" ]; then \
     # @4 - Clean repository - nothing to commit
-    echo "'$Green'"$(__git_ps1 " (%s)"); \
+    echo "'$Green'"$(__git_ps1 "%s"); \
   else \
     # @5 - Changes to working tree
-    echo "'$IRed'"$(__git_ps1 " {%s}"); \
+    echo "'$IRed'"$(__git_ps1 "%s"); \
   fi) '$BYellow$PathShort$Color_Off'\$ "; \
 else \
   # @2 - Prompt when not in GIT repo
