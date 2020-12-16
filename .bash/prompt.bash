89f3cfc24f28e3bd24c1fc6dc9aa4435db4273f8
Jonathan Trang <jon301@gmail.com>
2020-12-16 09:08:46 +0100

    Improve git prompt

--- .bash/prompt.bash
+++ .bash/prompt.bash
@@ -93,7 +93,7 @@ Jobs="\j"
 
 export PS1=$Color_Off'$(git branch &>/dev/null;\
 if [ $? -eq 0 ]; then \
-  echo "$(echo `git status` | grep "nothing to commit" > /dev/null 2>&1; \
+  echo "$(echo `git status` | grep "nothing to commit\|rien à valider" > /dev/null 2>&1; \
   if [ "$?" -eq "0" ]; then \
     # @4 - Clean repository - nothing to commit
     echo "'$Green'"$(__git_ps1 "%s"); \
